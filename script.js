ed105a7122f1bf775651d3fc1458c4c05b05883d
Vojislav Grujic <vojislav.grujic@gmail.com>
2019-10-07 14:45:05 +0200

    Use 'load' event for the image loading instead of fetch
    fetch does not work in a way that I expected

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const loadButton = document.querySelector('#load-image');
-const imageContainer = document.querySelector('#image-container');
-
 const imageNames = ['galaxies.png', 'bridge.jpg', 'stars.jpg', 'sombrero.jpg'];
 const numOfImages = imageNames.length;
 
@@ -11,33 +9,52 @@ loadButton.addEventListener('click', () => {
     const imageSource = `img/${imageNames[loadedImageIndex]}`;
     numberOfTimesLoaded++;
 
-    updateImageSource(imageSource, imageContainer);
+    updateImageSource(imageSource);
+    // updateImageSource(imageSource, imageContainer);
 });
 
-function updateImageSource(source, container) {
-    const image = container.querySelector('img');
+const imageContainer = document.querySelector('#image-container');
+const image = imageContainer.querySelector('img');
 
-    image.src = '';
-    image.classList.add('visible');
+image.addEventListener('load', () => {
+    // setTimeout(() => {
+        imageContainer.classList.remove('loading');
+        image.classList.add('visible');
+    // }, 500);
+});
 
-    container.classList.remove('loading-error');
-    container.classList.add('loading');
-
-    fetch(source)
-        .then(response => {
-            if (response.ok) {
-                image.src = source;
-            } else {
-                image.classList.remove('visible');
-                container.classList.add('loading-error');
-            }
-        })
-        .catch(error => {
-            console.error(error);
-            image.classList.remove('visible');
-            container.classList.add('loading-error');
-        })
-        .finally(() => {
-            container.classList.remove('loading');
-        });
+function updateImageSource(source) {
+    image.classList.remove('visible');
+    imageContainer.classList.add('loading');
+    image.src = source;
 }
+
+// Probati sa network throttle i manjim slikama
+
+// function updateImageSource(source, container) {
+//     const image = container.querySelector('img');
+//
+//     image.src = '';
+//     image.classList.add('visible');
+//
+//     container.classList.remove('loading-error');
+//     container.classList.add('loading');
+//
+//     fetch(source)
+//         .then(response => {
+//             if (response.ok) {
+//                 image.src = source;
+//             } else {
+//                 image.classList.remove('visible');
+//                 container.classList.add('loading-error');
+//             }
+//         })
+//         .catch(error => {
+//             console.error(error);
+//             image.classList.remove('visible');
+//             container.classList.add('loading-error');
+//         })
+//         .finally(() => {
+//             container.classList.remove('loading');
+//         });
+// }
